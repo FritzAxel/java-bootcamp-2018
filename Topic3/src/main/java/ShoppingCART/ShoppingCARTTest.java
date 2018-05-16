dab8a3772a83ba8648e589384afea7f0fc4e23c0
Axel Fritz <axel.fritz95@gmail.com>
2018-05-16 12:19:35 -0300

    Correction of the Topic2's directory and Topic3 created

---
+++ Topic3/src/main/java/ShoppingCART/ShoppingCARTTest.java
@@ -0,0 +1,5 @@
+package ShoppingCART;
+
+public class ShoppingCARTTest {
+
+}
---
+++ Topic3/src/test/java/ShoppingCART/ShoppingCART.java
@@ -0,0 +1,21 @@
+package ShoppingCART;
+/**
+ * This interface represents a service.
+ * 
+ * @author Axel Fritz
+ *
+ */
+public interface ShoppingCART {
+
+	/**
+	 * This method add an item to the CART.
+	 */
+	public void addItemToCart();
+	
+	/**
+	 * This method remove an item of the CART.
+	 */
+	public void removeItemOfCart();
+	
+	
+}
