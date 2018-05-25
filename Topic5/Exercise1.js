9553b2c26d560d60824fc4c4a5d2037a4723654d
Axel Fritz <axel.fritz95@gmail.com>
2018-05-24 22:07:02 -0300

    Topic5 Exercise1 finished

--- Topic5/Exercise1.js
+++ Topic5/Exercise1.js
@@ -31,6 +31,13 @@ db.student.insert([
    {"firstName": "Axel", "lastName": "Fritz", "registrationNumber": 1, "dateOfBirth": new Date("1995-08-26") },
    {"firstName": "Matias", "lastName": "Lopez", "registrationNumber": 2, "dateOfBirth": new Date("1984-05-30")},
    {"firstName": "Facundo", "lastName": "Moreyra", "registrationNumber": 3, "dateOfBirth": new Date("1996-07-20")},
+   {"firstName": "Gonzalo", "lastName": "Rocha","registrationNumber": 4, "dateOfBirth": new Date("1998-02-25")}
+   {"firstName": "Ricardo", "lastName": "Darin","registrationNumber": 5, "dateOfBirth": new Date("1986-06-05")}
+   {"firstName": "Victor", "lastName": "Lozano","registrationNumber": 6, "dateOfBirth": new Date("2000-05-04")}
+   {"firstName": "Hector", "lastName": "Chale","registrationNumber": 7, "dateOfBirth": new Date("1990-09-09")}
+   {"firstName": "Juliana", "lastName": "Izquierdo","registrationNumber": 8, "dateOfBirth": new Date("1989-10-17")}
+   {"firstName": "Silvina", "lastName": "Lopez","registrationNumber": 9, "dateOfBirth": new Date("1995-12-12")}
+   {"firstName": "Norberto", "lastName": "Azcuenaga","registrationNumber": 10, "dateOfBirth": new Date("1997-01-05")}
 ])
 
 
@@ -62,11 +69,10 @@ db.createCollection("teacher", {
 })
 
 db.teacher.insert([
-   {"id": ,"firstName": "", "lastName": "", "dateOfBirth": new Date()}
-   {"id": ,"firstName": "", "lastName": "", "dateOfBirth": new Date()}
-   {"id": ,"firstName": "", "lastName": "", "dateOfBirth": new Date()}
-   {"id": ,"firstName": "", "lastName": "", "dateOfBirth": new Date()}
-   {"id": ,"firstName": "", "lastName": "", "dateOfBirth": new Date()}
+   {"id": 1,"firstName": "Matias", "lastName": "Massaccesi", "dateOfBirth": new Date("1996-06-19")}
+   {"id": 2,"firstName": "Diego", "lastName": "Gregoracci", "dateOfBirth": new Date("1993-05-18")}
+   {"id": 3,"firstName": "Juan Pablo", "lastName": "Araneta", "dateOfBirth": new Date("1994-04-17")}
+
 ])
 
 
@@ -97,6 +103,12 @@ db.createCollection("course", {
    }
 })
 
+db.course.insert([
+   {"id": 1, "name": "Maths", "teacher_id": 1, "hoursByWeek": 4},
+   {"id": 2, "name": "Chemistry", "teacher_id": 2, "hoursByWeek": 8},
+   {"id": 3, "name": "Physics", "teacher_id": 3, "hoursByWeek": 6},
+])
+
 db.createCollection("note", {
    validator: {
       $jsonSchema: {
@@ -126,4 +138,38 @@ db.createCollection("note", {
       }
    }
 })
+
+db.note.insert([
+   {"type": "Partial", "score": 6, "course_id": 1, "student_id": 1},
+   {"type": "Partial", "score": 2, "course_id": 2, "student_id": 1},
+   {"type": "Partial", "score": 6, "course_id": 1, "student_id": 2},
+   {"type": "Partial", "score": 1, "course_id": 2, "student_id": 2},
+   {"type": "Partial", "score": 8, "course_id": 1, "student_id": 3},
+   {"type": "Final", "score": 6, "course_id": 2, "student_id": 3},
+   {"type": "Partial", "score": 10, "course_id": 2, "student_id": },
+   {"type": "Final", "score": 5  , "course_id": 3, "student_id": 1},
+   {"type": "Final", "score": 6, "course_id": 1, "student_id": },
+   {"type": "Partial", "score": 8, "course_id": 3, "student_id": 2},
+   {"type": "Partial", "score": 6, "course_id": 1, "student_id": },
+   {"type": "Final", "score": 9, "course_id": 3, "student_id": 3},
+   {"type": "Partial", "score": 8, "course_id": 1, "student_id": 4},
+   {"type": "Partial", "score": 5, "course_id": 1, "student_id": 4},
+   {"type": "Partial", "score": 5, "course_id": 1, "student_id": 4},
+   {"type": "Partial", "score": 8, "course_id": 1, "student_id": 5},
+   {"type": "Partial", "score": 5, "course_id": 1, "student_id": 5},
+   {"type": "Partial", "score": 5, "course_id": 1, "student_id": 5},
+   {"type": "Partial", "score": 5, "course_id": 1, "student_id": 6},
+   {"type": "Partial", "score": 8, "course_id": 1, "student_id": 6},
+   {"type": "Partial", "score": 5, "course_id": 1, "student_id": 6},
+   {"type": "Partial", "score": 5, "course_id": 2, "student_id": 7},
+   {"type": "Partial", "score": 5, "course_id": 2, "student_id": 7},
+   {"type": "Final", "score": 5, "course_id": 2, "student_id": 7},
+   {"type": "Final", "score": 8, "course_id": 2, "student_id": 8},
+   {"type": "Final", "score": 5, "course_id": 2, "student_id": 8},
+   {"type": "Final", "score": 8, "course_id": 2, "student_id": 9},
+   {"type": "Final", "score": 5, "course_id": 2, "student_id": 9},
+   {"type": "Final", "score": 8, "course_id": 2, "student_id": 2},
+   {"type": "Final", "score": 5, "course_id": 2, "student_id": 10},
+   {"type": "Final", "score": 8, "course_id": 2, "student_id": 1},
+])
 
